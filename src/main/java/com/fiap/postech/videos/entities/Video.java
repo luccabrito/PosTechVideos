3acb68629fb8ab5cd6b99d7df964bb57a178392b
Vinícius Miranda de Menezes <paravinicius@gmail.com>
2024-01-29 16:33:20 -0300

    Criado um gerardo de User na entity para reproveitar nos testes.

--- src/main/java/com/fiap/postech/videos/entities/Video.java
+++ src/main/java/com/fiap/postech/videos/entities/Video.java
@@ -1,12 +1,12 @@
 package com.fiap.postech.videos.entities;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 @Document(collection = "videos")
@@ -14,12 +14,14 @@ public class Video {
 
     @Id
     private String id;
+    @NotEmpty(message = "Título não informado.")
     private String titulo;
     private String descricao;
+    @NotEmpty(message = "URL não informada.")
     private String url;
     @Builder.Default //Regra de negócio: Garantir que a data já vai vir preenchida com a data corrente.
     private LocalDate dataDeUpload = LocalDate.now();
-    @DBRef
+    @DBRef @NotEmpty(message = "Usuário não informado.")
     private User uploadedBy;
     private Categoria categoria;
     @Builder.Default
