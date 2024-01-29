5a105c7b5ff2c50b95e35ad38feeace22aef1a7a
Vinícius Miranda de Menezes <paravinicius@gmail.com>
2024-01-29 15:58:58 -0300

    Todos os testes de user cases do package Favoritos em uma única classe de teste.

--- src/main/java/com/fiap/postech/videos/entities/Favorito.java
+++ src/main/java/com/fiap/postech/videos/entities/Favorito.java
@@ -1,18 +1,19 @@
 package com.fiap.postech.videos.entities;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor
 @Document(collection = "favoritos")
 public class Favorito {
 
     @Id
     private String id;
+    @NotEmpty(message = "O username do usuário deve ser informado.")
     private String username;
+    @NotEmpty(message = "O videoId deve ser informado informado.")
     private String videoId;
 
 }
---
+++ src/test/java/com/fiap/postech/videos/usecases/AllUseCasePackageFavoritoTests.java
@@ -0,0 +1,80 @@
+package com.fiap.postech.videos.usecases;
+
+import com.fiap.postech.videos.entities.Favorito;
+import com.fiap.postech.videos.repositories.FavoritoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class AllUseCasePackageFavoritoTests {
+
+    @Mock
+    private FavoritoRepository favoritoRepository;
+
+    AutoCloseable openMocks;
+
+    @BeforeEach
+        //Ao iniciar um teste, devem ser carregados todos os mocks;
+    void setup(){
+        openMocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+        //Ao finalizar um teste, todos os mocks devem ser desalocados da memória.
+    void tearDown() throws Exception{
+        openMocks.close();
+    }
+
+    @Test
+    void deveFavoritarVideo(){
+        // Arrange
+        Favorito favorito = gerarFavorito(1,1);
+        when(favoritoRepository.save(any(Favorito.class))).thenReturn(Mono.just(favorito));
+
+        //Act
+        Mono<Favorito> favoritoRetornadoMono = favoritoRepository.save(favorito);
+
+        //Assert
+        assertThat(favoritoRetornadoMono)
+                .isNotNull();
+        verify(favoritoRepository, times(1)).save(any(Favorito.class));
+    }
+
+    @Test
+    void deveObterVideosFavoritosPorUsernameUseCase(){
+        //Arrange
+        var favorito = gerarFavorito(1,1);
+        when(favoritoRepository.findByUsername(any(String.class))).thenReturn(Flux.just(favorito));
+
+        //Act
+        Flux<Favorito> favoritoRetornadoFlux = favoritoRepository.findByUsername(favorito.getUsername());
+
+        //Assert
+        assertThat(favoritoRetornadoFlux)
+                .isNotNull();
+        StepVerifier.create(favoritoRetornadoFlux)
+                .expectNextCount(0)//Gera "expectation "assertNext" failed" caso não retorne nada
+                .assertNext(favoritoRetornado -> {
+                    assertThat(favoritoRetornado.getUsername()).isEqualTo(favorito.getUsername());
+                })
+                .verifyComplete();
+    }
+
+    private Favorito gerarFavorito(int userId, int videoId){
+        return Favorito.builder()
+                .id(String.valueOf(userId))
+                .username("username_".concat(String.valueOf(userId)))
+                .videoId("videoId_".concat(String.valueOf(videoId)))
+                .build();
+    }
+
+}
